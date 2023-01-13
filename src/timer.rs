67c5d4eebceb06b540b0d14be87f56e29a89ce08
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2023-01-13 08:14:52 -0500

    Update timer.rs

--- src/timer.rs
+++ src/timer.rs
@@ -1,28 +1,26 @@
-pub mod fe {
-    pub mod timer {
-        use std::{ thread, time::Duration };
+pub mod timer {
+    use std::{ thread, time::Duration };
 
-        //Timer always starts at zero.
-        struct Timer {
-            is_stopped: bool,
-            current_time_ms: i32,
+    //Timer always starts at zero.
+    struct Timer {
+        is_stopped: bool,
+        current_time_ms: i32,
+    }
+
+    impl Timer {
+        fn start() {
+            while !is_stopped {
+                thread::sleep(Duration::from_millis(1));
+                current_time_ms += 1;
+            }
         }
 
-        impl Timer {
-            fn start() {
-                while !is_stopped {
-                    thread::sleep(Duration::from_millis(1));
-                    current_time_ms += 1;
-                }
-            }
+        fn stop() {
+            is_stopped = true;
+        }
 
-            fn stop() {
-                is_stopped = true;
-            }
-
-            fn reset() {
-                current_time_ms = 0;
-            }
+        fn reset() {
+            current_time_ms = 0;
         }
     }
 }
